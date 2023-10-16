dffffa76c050b9cb99fb7868f13236dccdfde641
Amelia Klapa <amelia.klapa@edu.uekat.pl>
2023-10-16 12:55:29 +0200

    new files

---
+++ zad2a.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+l=["Amelka","Agata","Wiktoria","Helena","Marek"]
+print(l)
+
+
+
---
+++ zad2d.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 12:26:08 2023
+
+@author: student
+"""
+
+import random
+lista = [random.randint(0,100) for x in range(10)]
+print(lista)
+co_druga = lista[::2]
+print(co_druga)
+
+    
